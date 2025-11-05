6b415e442c686d6a6ae720391e6281525b0444f3
nayeem165 <nayeem1656992@gmail.com>
2025-11-05 18:50:04 +0530

    initial commit

---
+++ Conditions.java
@@ -0,0 +1,11 @@
+public class Conditions{
+    public static void main(String args[]){
+        System.out.println("Hello World:");
+        int age = 16;
+
+if (age >= 18) {
+    System.out.println("You are eligible to vote.");
+}
+
+    }
+}
